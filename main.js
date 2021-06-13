5a8efbf521b668fd8e20fb6552da97b910dcf163
IvanTime-Kai <80437018+IvanTime-Kai@users.noreply.github.com>
2021-06-13 17:07:06 +0700

    es5

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// follow boolean dat bien theo nhu vay
+// null la kieu object ( day la bug cua JS )
+// khai bao hang so thi viet theo kieu : IN_HOA
+
+//note:
+// phai gan gia tri khi moi khoi tao bien
+var isStudent = true;
+console.log(isStudent);
+
+var kiemTra = '';
+console.log(typeof kiemTra);
